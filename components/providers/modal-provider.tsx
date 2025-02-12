da7132e7d91ecf35114e8f6cfb0e47456df120dd
Souvik606 <mukherjeesouvik606@gmail.com>
2025-02-12 23:31:49 +0530

    messages component and edit delete messages features added

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -10,6 +10,7 @@ import { DeleteServerModal } from "../modals/delete-server-modal"
 import { DeleteChannelModal } from "../modals/delete-channel-modal"
 import { EditChannelModal } from "../modals/edit-channel-modal"
 import { MessageFileModal } from "../modals/message-file"
+import { DeleteMessageModal } from "../modals/delete-message-modal"
 
 export const ModalProvider=()=>{
     return (
@@ -24,6 +25,7 @@ export const ModalProvider=()=>{
         <DeleteChannelModal/>
         <EditChannelModal/>
         <MessageFileModal/>
+        <DeleteMessageModal/>
         </>
     )
 }
