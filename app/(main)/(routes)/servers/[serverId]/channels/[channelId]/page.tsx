5ee3e27b60eefb437198c9ecc36b223dd8d7f45b
Souvik606 <mukherjeesouvik606@gmail.com>
2025-02-13 13:11:52 +0530

    build errors fixed

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -17,6 +17,7 @@ interface ChannelIdPageProps {
 
 const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   const profile = await currentProfile()
+  const {serverId,channelId}=await params;
 
   if (!profile) {
     return redirect('/sign-in')
@@ -24,13 +25,13 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
 
   const channel = await db.channel.findUnique({
     where: {
-      id: params.channelId,
+      id: channelId,
     },
   })
 
   const member = await db.member.findFirst({
     where: {
-      serverId: params.serverId,
+      serverId: serverId,
       profileId: profile.id,
     },
   })
